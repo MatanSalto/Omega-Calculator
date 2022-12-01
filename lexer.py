161398fa8060bbeef21dd1a817aaa9c1a63b1b6a
unknown <saltomtn9@gmail.com>
2022-12-01 13:28:15 +0200

    Add Lexer class

--- lexer.py
+++ lexer.py
@@ -28,64 +28,65 @@ class TokenStream:
         return False
 
 
+class Lexer:
 
-def lex_input_string(string: str) -> list[Token]:
+    def lex_input_string(self, string: str) -> list[Token]:
 
-    # Initialize the list of tokens
-    tokens = []
-    
-    # Initialize the string index
-    index = 0
-
-    # Loop through the characters in the string
-    while string[index] != '\0':
-        # If the character is an operator, append it to the token list as a character
-        if string[index] in OPERATORS:
-            tokens.append(string[index])
-            index += 1
+        # Initialize the list of tokens
+        tokens = []
         
-        # Else, if the character is a digit, continute reading the digits until reaching a non-digit character
-        elif string[index].isdigit():
-            number = 0
-            digits_after_decimal_point = 0 # counter to indicate how many digits there are after the decimal place
+        # Initialize the string index
+        index = 0
 
-            # Append the digit to the number until the character is not a digit
-            while string[index].isdigit():
-                number = number * 10 + int(string[index])
+        # Loop through the characters in the string
+        while string[index] != '\0':
+            # If the character is an operator, append it to the token list as a character
+            if string[index] in OPERATORS:
+                tokens.append(string[index])
                 index += 1
+            
+            # Else, if the character is a digit, continute reading the digits until reaching a non-digit character
+            elif string[index].isdigit():
+                number = 0
+                digits_after_decimal_point = 0 # counter to indicate how many digits there are after the decimal place
 
-            # If the character is a decimal point, continue reading the characters after it
-            if string[index] == '.':
-                index += 1
-        
+                # Append the digit to the number until the character is not a digit
                 while string[index].isdigit():
-                    # Append the digit to the number
                     number = number * 10 + int(string[index])
-                    # Increment the counter 
-                    digits_after_decimal_point += 1
-                    # Increment the index
                     index += 1
 
-                # At this point, if there are no digits after the decimal place, the number is not valid (it is a number like 2342.)
-                if digits_after_decimal_point == 0:
-                    print(f"Expected a digit at index {index}")
-                    return
+                # If the character is a decimal point, continue reading the characters after it
+                if string[index] == '.':
+                    index += 1
+            
+                    while string[index].isdigit():
+                        # Append the digit to the number
+                        number = number * 10 + int(string[index])
+                        # Increment the counter 
+                        digits_after_decimal_point += 1
+                        # Increment the index
+                        index += 1
 
-                # Else, calculate the number and add the corresponding token to the list
-            tokens.append(Number(number * (10 ** -digits_after_decimal_point)))
+                    # At this point, if there are no digits after the decimal place, the number is not valid (it is a number like 2342.)
+                    if digits_after_decimal_point == 0:
+                        print(f"Expected a digit at index {index}")
+                        return
 
-        # Else, the character is not valid
-        else:
-            print(f"Non valid symbol at index {index}")
-            return
+                    # Else, calculate the number and add the corresponding token to the list
+                tokens.append(Number(number * (10 ** -digits_after_decimal_point)))
 
-    return TokenStream(tokens[::-1])
+            # Else, the character is not valid
+            else:
+                print(f"Non valid symbol at index {index}")
+                return
+
+        return TokenStream(tokens)
 
 
 
 if __name__ == "__main__":
 
-    string = "3 + s5 - 12.3/20\0"
+    string = "3 + 5 - 12.3/20\0"
 
     tokens = lex_input_string(string)
 
