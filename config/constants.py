350a8c0f7b600524cd25c842ab03f075e6d417bb
unknown <saltomtn9@gmail.com>
2022-12-09 17:40:51 +0200

    Add config folder

---
+++ config/constants.py
@@ -0,0 +1,18 @@
+from tokens import *
+
+# Valid operators
+OPERATORS = {
+    '+':Plus,
+    '-':Minus,
+    '*':Mult,
+    '/':Div,
+    '^':Power,
+    '%':Mod,
+    '$':Max,
+    '&':Min,
+    '@':Avg,
+    '~':Tilda,
+    '!':Factorial,
+    '#':SumDigits,
+    '(':OpenParen,
+    ')':CloseParen}
