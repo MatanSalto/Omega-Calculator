64657010f8575766f84e976482e58421f4fff74a
unknown <saltomtn9@gmail.com>
2022-12-09 17:38:46 +0200

    Add validate method to operators

--- tokens.py
+++ tokens.py
@@ -25,6 +25,10 @@ class Operator(Token):
     def __init__(self, index:int, value) -> None:
         super().__init__(index, "Operator", value)
 
+    # Method to validate the operands. Returns True by default (if not overridden)
+    def validate_operands(self) -> bool:
+        return True
+
 
 class OpenParen(Operator):
     def __init__(self, index: int, value) -> None:
@@ -54,18 +58,24 @@ class Factorial(UnaryOperator):
 
     def evaluate(self) -> float:
         # Evaluate the operand
-        value = self.operand.evaluate()
+        self.value = self.operand.evaluate()
 
-        # If the value is not a natural number, raise an exception
-        if not(value >= 0 and value % 1 == 0):
-            raise InvalidOperandException("Invalid operand for the factorial operator. Only natural numbers are allowed")
-
-        # Else, evaluate the expression
-        return self._factorial(value)
+        # Validate the operands
+        if self.validate_operands():
+            return self._factorial(self.value)
 
     def _factorial(self, n:int) -> int:
         if n == 0: return 1
         return n * self._factorial(n-1)
+
+    def validate_operands(self) -> bool:
+        
+         # If the value is not a natural number, raise an exception
+        if not(self.value >= 0 and self.value % 1 == 0):
+            raise InvalidOperandException("Invalid operand for the factorial operator. Only natural numbers are allowed")
+        
+        # Else, return True
+        return True
 
 
 class Tilda(UnaryOperator):
@@ -73,10 +83,17 @@ class Tilda(UnaryOperator):
         super().__init__(index, value, operand)
 
     def evaluate(self) -> float:
+        # Validate the operands
+        if self.validate_operands():
+            return -1 * self.operand.evaluate()
+
+    def validate_operands(self) -> bool:
         # If the operand is consist non-numbers tokens, raise an exception
         if not self._check_children(self.operand):
             raise InvalidOperandException("Invalid operand for the tilda operator. Only numbers are allowed, and not expressions")
-        return -1 * self.operand.evaluate()
+
+        # Else, return True
+        return True
 
     def _check_children(self, node):
         # Base case: if the current node is null or a number, return true
@@ -104,21 +121,15 @@ class SumDigits(UnaryOperator):
         super().__init__(index, value, operand)
 
     def evaluate(self) -> float:
-        # Evaluate the operand
-        value = self.operand.evaluate()
-
-        # If the operand it negative, raise an exception
-        if value < 0:
-            raise InvalidOperandException("Invalid operand for sum-digits operaor. Only non-negative numbers are allowed")
-        return self._count_digits(value)
+        return self._count_digits(self.operand.evaluate())
 
     def _count_digits(self, num):
         sum = 0
         for digit in str(num):
-            if digit != '.':
+            if digit != '.' and digit != '-':
                 sum += int(digit)
 
-        return sum
+        return sum if num > 0 else -sum
 
 class Plus(BinaryOperator):
     def __init__(self, index:int, value: str, left: Token = None, right: Token = None) -> None:
@@ -150,15 +161,20 @@ class Div(BinaryOperator):
 
     def evaluate(self) -> float:
         # Evaluate the operands
-        left_value = self.left.evaluate()
-        right_value = self.right.evaluate()
-
-        # If the right operand is 0, raise an exception
-        if right_value == 0:
-            raise InvalidOperandException("Invalid operand for division operator. Cannot divide by 0")
+        self.left_value = self.left.evaluate()
+        self.right_value = self.right.evaluate() 
         
-        # Else, evaluate the expression
-        return left_value / right_value
+        # Validate the operands
+        if self.validate_operands():
+            return self.left_value / self.right_value
+
+    def validate_operands(self) -> bool:
+        # If the right operand is 0, raise an exception
+        if self.right_value == 0:
+            raise InvalidOperandException("Invalid operand for division operator. Cannot divide by 0")
+
+        # Else, return True
+        return True
 
 
 class Power(BinaryOperator):
@@ -167,15 +183,20 @@ class Power(BinaryOperator):
 
     def evaluate(self) -> float:
         # Evaluate the operands
-        left_value = self.left.evaluate()
-        right_value = self.right.evaluate()
+        self.left_value = self.left.evaluate()
+        self.right_value = self.right.evaluate()
 
+        # Validate the operands
+        if self.validate_operands():
+            return self.left_value ** self.right_value
+
+    def validate_operands(self) -> bool:
         # If the expression is 0^0, raise an exception
-        if left_value == 0 and right_value == 0:
+        if self.left_value == 0 and self.right_value == 0:
             raise InvalidOperandException("Invalid operand for power expression. Cannot evaluate 0^0")
         
-        # Else, evaluate the expression
-        return left_value ** right_value
+        # Else, return True
+        return True
 
 
 class Mod(BinaryOperator):
@@ -184,13 +205,20 @@ class Mod(BinaryOperator):
 
     def evaluate(self) -> float:
         # Evaluate the operands
-        left_value = self.left.evaluate()
-        right_value = self.right.evaluate()
+        self.left_value = self.left.evaluate()
+        self.right_value = self.right.evaluate()
 
+        # Validate the operands
+        if self.validate_operands():
+            return self.left_value % self.right_value
+
+    def validate_operands(self) -> bool:
         # If the right side is 0, raise an exception
-        if right_value == 0:
+        if self.right_value == 0:
             raise InvalidOperandException("Invalid operand for mod operator. Cannot divide by 0")
-        return left_value % right_value
+
+        # Else, return True
+        return True
 
 
 class Max(BinaryOperator):
