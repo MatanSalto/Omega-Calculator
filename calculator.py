c79bc2f51e11ae9e51db0fab749d08d4d5eb0559
unknown <saltomtn9@gmail.com>
2022-12-10 10:19:53 +0200

    complete calculator class structure

--- calculator.py
+++ calculator.py
@@ -1,9 +1,35 @@
 from lexer import Lexer
-from parsing import Parser
+from parsing import Parser, RecursiveDescentParser
+from IO.input import InputReader, ConsoleReader
+from IO.output import OutputPrinter, ConsoleOutputPrinter
+
 
 class Calculator:
 
     def __init__(self) -> None:
+        self.reader : InputReader = ConsoleReader() # Input device
+        self.lexer : Lexer = Lexer() # Lexer object
+        self.parser : Parser = RecursiveDescentParser() # Parser object
+        self.printer : OutputPrinter = ConsoleOutputPrinter() # Output device
 
-        self.lexer = Lexer()
-        self.parser = Parser()
+    def activate(self) -> None:
+        # Get the input string
+        string = self.reader.input("Enter the expression string:\n") + '\0'
+
+        # Lex the input string
+        tokens = self.lexer.lex(string)
+
+        # Parse the sequence of tokens
+        expression_node = self.parser.parse(tokens, string)
+
+        # Evaluate the expression
+        result = expression_node.evaluate()
+
+        # Output the result to the user
+        self.printer.output(f"The result is {result}")
+
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.activate()
