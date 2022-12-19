872e7252436c1ed6ccce8288d5e1b038ba1c7cae
Matan Salto <saltomtn9@gmail.com>
2022-12-19 11:28:33 +0200

    Change the mainloop of the calculator

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
+from Exceptions.exceptions import *
 from IO.input import ConsoleReader, InputReader
 from IO.output import ConsoleOutputPrinter, OutputPrinter
 from lexer import Lexer
 from parsing import Parser, RecursiveDescentParser
-from Exceptions.exceptions import *
 
 
 class Calculator:
@@ -13,68 +13,76 @@ class Calculator:
         self.parser : Parser = RecursiveDescentParser() # Parser object
         self.printer : OutputPrinter = ConsoleOutputPrinter() # Output device
 
-    def activate(self) -> None:
+    def activate(self) -> bool:
+        """
+        This function performs the whole calculation process.
+        It gets the input from the user, evaluated the exception and outputs th result
+        Returns:
+            bool: True if the calculation was successfull, and false otherwise
+        """
         # Try getting the input string
         try:
             string = self.reader.input("Enter the expression string:\n") + '\0'
+            tokens = self.lexer.lex(string)
+            expression_node = self.parser.parse(tokens, string)
+            result = expression_node.evaluate()
+            self.printer.output(f"The result is {result}")
+            return True
+
         # Catch input excpetions
         except ParsingInterrupt as pi:
             # Print the message
             print(pi)
-            # Start the function again
-            return self.activate()
+            # Return False
+            return False
 
-
-        # Try lexing the string
-        try:
-            tokens = self.lexer.lex(string)
         # Catch invalid symbol exception
         except InvalidSymbolException as ise:
             # Print the message
             print(ise)
-            # Start the function again
-            return self.activate()
+            # Return False
+            return False
+
         # Catch empty string exception
         except EmptyInputString as eis:
             # Print the message
             print(eis)
-            # Start the function again
-            return self.activate()
+            # Return False
+            return False
 
-        
-        # Try parsing the expression
-        try:
-            expression_node = self.parser.parse(tokens, string)
         # Catch syntax exceptions
         except SyntaxException as se:
             # Print the message
             print(se)
-            # Start the function again
-            return self.activate()
+            # Return False
+            return False
 
-        # Try evaluating the expression
-        try:
-            result = expression_node.evaluate()
         # Catch invalid operand exception
         except InvalidOperandException as ioe:
             # Print the message
             print(ioe)
-            # Call the function again
-            return self.activate()
+            # Return False
+            return False
 
+        # Catch overflow error
+        except OverflowError as ofe:
+            # Print the message
+            print(ofe)
+            # Return False
+            return False
         
-        # Try printing the result to the user
-        try:
-            self.printer.output(f"The result is {result}")
         # Catch output exception
         except ParsingInterrupt as pi:
             # Print the message
             print(pi)
-            # Start the function again
-            return self.activate()
+            # Return False
+            return False
 
 
 if __name__ == "__main__":
-    # Create a calculator instance and activate it
+    # Create a calculator instance
     calc = Calculator()
-    calc.activate()
+    
+    # Activate the calculator over and over again until the calculation is successfull
+    while not calc.activate():
+        pass
